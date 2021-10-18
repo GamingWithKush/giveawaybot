7b8ac65819a07238a64108a168699074f881f907
GamingWithKush <84759694+GamingWithKush@users.noreply.github.com>
2021-10-18 10:05:22 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+Add Your Token In Secrets
+Add your prefix in config.json
+## Features
+* Create your own custom giveaways!
+* Custom time!
+* Custom prize!
+* Custom role mention!
+* Multiple winners!
+* Not manipulable!
---
+++ index.js
@@ -0,0 +1,46 @@
+const { Client, Collection } = require("discord.js");
+const config = require("./config.json");
+const client = new Client();
+const { GiveawaysManager } = require('discord-giveaways');
+
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
+    console.log(`Giveaway #${giveaway.messageID} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
+});
+
+
+
+["aliases", "commands"].forEach(x => client[x] = new Collection());
+["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Bot working!')
+})
+
+app.listen(port, () => {
+  console.log(`app listening at port ${port}`)
+})
+
+
+client.login(process.env.TOKEN);
